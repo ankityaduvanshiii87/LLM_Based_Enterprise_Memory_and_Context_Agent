5472bb0388e1ac634e688eb3a1318c2585ed217d
Ankit Yadav <ankityaduvanshiii@gmail.com>
2025-11-22 23:36:40 +0530

    Folder Structure Added

---
+++ templete.sh
@@ -0,0 +1,15 @@
+# Create the directory
+mkdir -p src
+mkdir -p research
+
+# create the files
+touch src/__init__.py
+touch src/helper.py 
+touch src/prompt.py
+touch .enve
+touch setup.py
+touch app.py
+touch research/trails.py
+touch requirements.txt
+ 
+echo "Directory amd files created Successfully!"
