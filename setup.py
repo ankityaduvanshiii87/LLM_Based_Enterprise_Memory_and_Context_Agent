93063280c25808694872952bf90b67dcac688570
Ankit Yadav <ankityaduvanshiii@gmail.com>
2025-11-23 00:21:47 +0530

    requirements Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import  find_packages,setup
+
+setup(
+    name="ChatBot",
+    version="0.1.0",
+    author="Ankit yadav",
+    author_email="ay076609@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
